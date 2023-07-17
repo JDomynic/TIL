35954936b53065800ef2d404a6b2383546997afa
진창현 <chang5189@naver.com>
2023-07-17 12:22:57 +0900

    TIL

---
+++ 1강_연습.py
@@ -0,0 +1,61 @@
+"""
+a1 = 1
+a2 = 2.3
+a3 = 2 + 3j
+a4 = "Hello World"
+a5 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # 알고리즘문제에서 많이 사용
+a6 = (1, 2, 3, 4)
+a7 = {1, 2, 3}
+a8 = {'one' : 1, 'two' : 2, 'three' : 3} # 웹에서 많이 사용
+a9 = True
+a10 = None
+a11 = lambda x, y : x + y
+a12 = range(10)
+a13 = {"a1" : [1, 2, 3], "a2" : [4, 5, 6]}
+
+for i in range(1, 14):
+    var = f'a{i}'
+    var = eval(var)
+    print(type(var))"""
+    
+
+# print("7 / 3 : ", 7 / 3, "\n7 // 3 : ", 7 // 3, "\n7 % 3 : ", 7 % 3)
+
+
+# print(-2 ** 4)
+# print(-(2 ** 4))
+# print((-2) ** 4)
+
+
+# degrees = 36.5
+# print(id(degrees))
+
+# degrees = 37.5
+# print(id(degrees))
+
+
+# num = 10
+# double = 2 * num
+# print(double)
+
+# num = 5
+# print(double)
+
+# name = "min"
+# age = 33
+# is_student = False
+# SECOND = 60
+# MAX_VALUE = 100
+# PI = 3.14159
+
+# person_name = "Alice"
+
+# def calculate_sum(x, y):
+#     return x + y
+
+
+# def calculate_sub(x, y):
+#     return x - y
+
+
+## PEP8
